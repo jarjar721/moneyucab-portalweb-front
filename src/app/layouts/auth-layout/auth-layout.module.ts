03897df3c194a76e03807435428166fb93816bce
José Andrés Rodríguez <jose@crewnew.com>
2020-04-16 13:55:53 -0400

    creacion y routing de la vista "pw-recovery"

--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -7,6 +7,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LoginComponent } from '../../pages/login/login.component';
 import { RegisterComponent } from '../../pages/register/register.component';
+import { PasswordRecoveryComponent } from '../../pages/pw-recovery/pw-recovery.component';
+
 @NgModule({
   imports: [
     CommonModule,
@@ -16,7 +18,8 @@ import { RegisterComponent } from '../../pages/register/register.component';
   ],
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    PasswordRecoveryComponent
   ]
 })
 export class AuthLayoutModule { }
---
+++ src/app/pages/pw-recovery/pw-recovery.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-pw-recovery',
+  templateUrl: './pw-recovery.component.html',
+  styleUrls: ['./pw-recovery.component.scss']
+})
+export class PasswordRecoveryComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit() {
+  }
+  ngOnDestroy() {
+  }
+
+}
