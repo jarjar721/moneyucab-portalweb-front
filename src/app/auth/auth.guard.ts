fdc99e58a1716f5a77744b120cd3d960d26e2097
José Andrés Rodríguez <jose@crewnew.com>
2020-04-25 12:38:37 -0400

    added validation if token is expired

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
   constructor(private router: Router) {
-
   }
+
 
   /*
   * FUNCION: canActivate()
@@ -20,15 +22,20 @@ export class AuthGuard implements CanActivate {
   * -- Si encuentra el token, devuelve TRUE y permite el acceso.
   * -- Si no encuentra el token, redirige al usuario al login page.
   */
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-      if(localStorage.getItem('token') != null){
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+
+      const helper = new JwtHelperService();
+      var token = localStorage.getItem('token');
+
+      if(token != null && !helper.isTokenExpired(token)){
         return true;
       } else {
+        if(helper.isTokenExpired(token)) // test
+          console.log('Token expired!'); // test
         this.router.navigate(['/login']);
         return false;
       }
+      
   }
   
 }
