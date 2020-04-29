3377ca7f81a8941cfb065627e124d2eea66d1127
José Andrés Rodríguez <jose@crewnew.com>
2020-04-28 21:59:51 -0400

    fixed landing 100% but now errors in styles

--- src/assets/js/landingpage.js
+++ src/assets/js/landingpage.js
@@ -1,16 +1,17 @@
 // HEADER ANIMATION
 window.onscroll = function() {scrollFunction()};
-var element = document.getElementById("body");
+//var element = document.getElementById("body");
+var element = document.body;
 function scrollFunction() {
   if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
       $(".navbar").addClass("fixed-top");
       element.classList.add("header-small");
-      $("body").addClass("body-top-padding");
+      $("#body").addClass("body-top-padding");
 
   } else {
       $(".navbar").removeClass("fixed-top");
       element.classList.remove("header-small");
-      $("body").removeClass("body-top-padding");
+      $("#body").removeClass("body-top-padding");
   }
 }
 
@@ -40,12 +41,12 @@ $('.owl-carousel').owlCarousel({
 $(document).ready(function() {
   $(".nav-link").click(function() {
       var t = $(this).attr("href");
-      $("html, body").animate({
+      $("html, #body").animate({
           scrollTop: $(t).offset().top - 75
       }, {
           duration: 1000,
       });
-      $('body').scrollspy({ target: '.navbar',offset: $(t).offset().top });
+      $('#body').scrollspy({ target: '.navbar',offset: $(t).offset().top });
       return false;
   });
 
@@ -65,18 +66,18 @@ AOS.init({
 
 //SIDEBAR-OPEN
   $('#navbarSupportedContent').on('hidden.bs.collapse', function () {
-    $("body").removeClass("sidebar-open");
+    $("#body").removeClass("sidebar-open");
   })
 
   $('#navbarSupportedContent').on('shown.bs.collapse', function () {
-    $("body").addClass("sidebar-open");
+    $("#body").addClass("sidebar-open");
   })
 
 
   window.onresize = function() {
     var w = window.innerWidth;
     if(w>=992) {
-      $('body').removeClass('sidebar-open');
+      $('#body').removeClass('sidebar-open');
       $('#navbarSupportedContent').removeClass('show');
     }
   }
